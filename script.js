9e8f341c0b18f4391510f44cdb93f7ea335128bc
ClauderPaz87 <142545565+ClauderPaz87@users.noreply.github.com>
2025-01-01 20:29:23 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,16 @@
+/*let validar = document.querySelector("#form-user-create [name = name]")
+
+validar.value = 'Java'
+*/
+/*document.querySelectorAll('button').forEach(function(btn){
+
+    btn.addEventListener('click', function(){
+
+        alert('Clicou')
+
+    })
+
+})
+*/
+
+let userController = new UserController("form-user-create", "form-user-update", "table-users")
